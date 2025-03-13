28596b5c39cb9253ffe4fe4cbc13bd98e4853afe
Russellg <dudurussell@gmail.com>
2025-03-13 09:58:34 +0200

    feat, fix: add CORS and tracing layers to test client; sleep only for the set sleep amount

--- test_client/src/main.rs
+++ test_client/src/main.rs
@@ -2,13 +2,17 @@ use std::{collections::HashMap, time::Duration};
 
 use axum::{extract::Query, response::IntoResponse, routing::get, Router};
 use tokio::net::TcpListener;
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let app = Router::new().route("/", get(sleep_handler));
+    let app = Router::new()
+        .route("/", get(sleep_handler))
+        .layer(TraceLayer::new_for_http())
+        .layer(CorsLayer::very_permissive());
 
-    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!(
         "server running on port {}",
         listener
@@ -22,7 +26,7 @@ async fn main() {
 async fn sleep_handler(Query(params): Query<HashMap<String, u64>>) -> impl IntoResponse {
     let timeout_length = params.get("timeout").expect("timeout parameter").to_owned();
     let sleep_length = params.get("sleep").expect("sleep parameter").to_owned();
-    let duration = Duration::from_secs(timeout_length + sleep_length);
+    let duration = Duration::from_secs(sleep_length);
 
     tokio::time::sleep(duration).await;
 
